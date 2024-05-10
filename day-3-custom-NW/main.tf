4c02c0978bff4c48e8f21a6f262730bfd990a4b6
syedafzal786 <syedafzal22334@gmail.com>
2024-05-10 09:21:23 +0530

    terraform updated file

--- day-3-custom-NW/main.tf
+++ day-3-custom-NW/main.tf
@@ -38,17 +38,46 @@ resource "aws_route_table_association" "dev" {
   route_table_id = aws_route_table.dev.id
 
 }
+# creation of sg
+resource "aws_security_group" "allow_tls" {
+  name   = "allow_tls"
+  vpc_id = aws_vpc.dev.id
+  tags = {
+    Name = "dev_sg"
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
 # creation of ec2
 resource "aws_instance" "Name" {
   ami           = "ami-013e83f579886baeb"
   instance_type = "t2.micro"
   key_name      = "syed2"
   subnet_id     = aws_subnet.dev.id
-  
-
+  vpc_security_group_ids = [aws_security_group.dev_sg.id]
+     
   tags = {
     Name = "myec2"
   }
-
 }
+
 
