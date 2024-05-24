d331c43e0d6fc12deacd300065660f849acbda05
syedafzal786 <168406355+syedafzal786@users.noreply.github.com>
2024-05-24 23:27:13 +0530

    Update main.tf

--- day-9-ec2-keypair/main.tf
+++ day-9-ec2-keypair/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "test" {
   key_name = "public"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file("~/syeda/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "syed" {
