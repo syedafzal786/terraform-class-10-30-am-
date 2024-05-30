2aa1bc3be8ef283c1296f4dcccc4d68ee04ccb64
syedafzal786 <syedafzal22334@gmail.com>
2024-05-30 13:58:06 +0530

    update

--- day-9-ec2-userdata/test.sh
+++ day-9-ec2-userdata/test.sh
@@ -23,3 +23,7 @@ sudo systemctl start jenkins
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+# -----------------install maven --------------------------
+
+yum install maven -y
