1eeb4e5b80dafe4196df05e68bf22cc4bd1330a2
Syed Afzal <168406355+syedafzal786@users.noreply.github.com>
2024-07-29 13:12:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ sudo dnf install java-17-amazon-corretto -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-sudo yum upgrade
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
